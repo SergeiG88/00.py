93d5dfb9bb318feaff13e339ad40955f5d9f7516
SergeiG88 <165669780+SergeiG88@users.noreply.github.com>
2024-04-13 17:10:27 +0200

    Add files via upload

--- Декараторы Дом.py
+++ Декараторы Дом.py
@@ -1,49 +1,19 @@
-# def my_decor(func):
-#     def wrapper():
-#         print('start')
-#         func()
-#         print('end')
-#     return wrapper
-#
-# def my_func():
-#     print('Тут основная функция')
-#
-#
-# my = my_decor(my_func)
-# # print(my)
-# my()
+def is_prime(func):
+
+    def wrapper(*n):
+        print('Простое')
+        func(*n)
+        # print('Составное')
+    return wrapper
+
+@is_prime
+def sum_three(x,y,z):
+    print(x + y + z)
 
 
-# def my_decor(func):
-#     def wrapper(n):
-#         print('start')
-#         func(n)
-#         print('end')
-#
-#     return wrapper
-#
-# @my_decor
-# def my_func(number):
-#     print(number ** 2)
-#
-#
-# my_func(10)
+# result = sum_three(2,3,6)
+# print(result)
+sum_three(2,3,6)
 
-import time
-
-def my_decor(func):
-    def my_wr():
-        start_time = time.time()
-        func()
-        print(time.time() - start_time)
-    return my_wr()
-
-@my_decor
-def sp():
-    spisok = [i ** 2 for i in range(100000) if i % 2 == 0]
-    print(spisok)
-
-# sp()
-print(my_decor)
 
 
